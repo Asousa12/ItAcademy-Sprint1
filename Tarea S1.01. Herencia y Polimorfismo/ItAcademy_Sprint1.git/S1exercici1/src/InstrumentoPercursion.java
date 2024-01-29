026eef3fd04e3d60e52a323811b2a5fcff7096ef
Asousa12 <148540078+Asousa12@users.noreply.github.com>
2024-01-29 11:15:03 +0100

    Delete Tarea S1.01. Herencia y Polimorfismo directory

--- Tarea S1.01. Herencia y Polimorfismo/ItAcademy_Sprint1.git/S1exercici1/src/InstrumentoPercursion.java
+++
@@ -1,9 +0,0 @@
-public class InstrumentoPercursion extends InstrumentosMusicales{
-
-    public InstrumentoPercursion(String nombre, double precio){
-        super(nombre, precio);
-    }
-    public String tocar() {
-        return "Está sonando un instrumento de percusión";
-    }
-}
--- Tarea S1.01. Herencia y Polimorfismo/src/n2exercici1/Reloj.java
+++
@@ -1,7 +0,0 @@
-package n2exercici1;
-
-public interface Reloj {
-	
-	void alarma();
-
-   }
